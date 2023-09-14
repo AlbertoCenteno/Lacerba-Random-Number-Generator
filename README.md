8ed7474a26a4aace7b99cd6eb02394309676522b
AlbertoCenteno <j.alberto.centeno.p@gmail.com>
2023-09-14 15:15:13 +0200

    Backend Finish

--- README.md
+++ README.md
@@ -3,3 +3,33 @@
 this is a simple fullstack random number generator
 program developed by @alberto (me) during the 
 Lacerba node and typesript development course.
+
+## Install
+
+```bash
+npm install
+```
+
+## usage backend
+
+```bash
+ts-node main.ts
+```
+
+example output:
+
+```
+{
+    Bari: [ 92, 26, 37, 12, 53 ],
+    cagliari: [ 79, 8, 82, 61, 6 ],
+    Firenze: [ 33, 1, 57, 48, 31 ],
+    Genova: [ 43, 2, 60, 84, 73 ],
+    Milano: [ 16, 26, 3, 85, 88 ],
+    Napoli: [ 68, 49, 95, 64, 26 ],
+    Palermo: [ 32, 29, 4, 90, 61 ],
+    Roma: [ 89, 95, 18, 4, 94 ],
+    Torino: [ 32, 80, 2, 73, 54 ],
+    Venezia: [ 36, 4, 63, 61, 44 ],
+    Nazionale: [ 55, 75, 25, 31, 44 ]
+}
+```
--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-console.log('ciao mondo');
+function RNG(min: number, max: number){
+    const rng = Math.random();
+    return Math.trunc(rng * (max - min) + min);
+}
+
+function RNGSequence(len: number, min: number, max: number){
+
+    if (len > max - min) {
+        throw new Error(`can't find ${len} numbers between ${min} and ${max}`);
+    }
+    const result: number[] = [];
+    
+    while(result.length < len){
+        const rn = RNG(min, max);
+        if(result.includes(rn)){
+            continue;
+        }
+        result.push(rn);
+    }
+    return result;
+}
+
+const wheels = ['Bari', 'cagliari', 'Firenze', 'Genova', 'Milano', 'Napoli', 'Palermo', 'Roma', 'Torino', 'Venezia', 'Nazionale'];
+const extractions: {[ruota: string]: number[]} = {};
+
+for(const ruota of wheels){
+    const extraction = RNGSequence(5, 1, 100);
+    extractions[ruota] = extraction;
+}
+
+console.log(extractions);
